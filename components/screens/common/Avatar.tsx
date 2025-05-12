a376ee16536bfe5e1c6bea3d9915fa9c5f662465
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-05-12 15:24:18 +0200

    fix: fix avatars

--- components/screens/common/Avatar.tsx
+++ components/screens/common/Avatar.tsx
@@ -1,8 +1,12 @@
 import { ThemedText } from "@/components/ui/ThemedText"
 import { ThemedView } from "@/components/ui/ThemedView"
+import { backend } from "@/lib/services/backend"
 import { BACKEND_URL } from "@/lib/services/backend/url"
 import type React from "react"
 import { Image, StyleSheet, View } from "react-native"
+import { bufferToUri } from "../chat/bufferToUri"
+import { useBackend } from "@/lib/services/backend/use"
+import { useState } from "react"
 
 interface AvatarProps {
   avatar: string | null // Avatar can be a string (URL) or null
@@ -12,10 +16,27 @@ interface AvatarProps {
 export const Avatar: React.FC<AvatarProps> = ({ avatar, username }) => {
   const uri = `${BACKEND_URL!}/files/${avatar}`
 
+  const [avatarUri, setAvatarUri] = useState<string | undefined>(undefined)
+  const { loading: imageLoading } = useBackend<ArrayBuffer>(
+    () => {
+      if (!avatar) return Promise.resolve(null)
+      return backend.files[avatar].get()
+    },
+    [avatar],
+    {
+      transform: (response) => {
+        if (response.data) {
+          const url = bufferToUri(response.data)
+          setAvatarUri(url)
+          return url
+        }
+      }
+    }
+  )
   return (
     <View style={styles.avatarContainer}>
-      {avatar ? (
-        <Image source={{ uri }} style={styles.avatar} />
+      {avatarUri ? (
+        <Image source={{ uri: avatarUri }} style={styles.avatar} />
       ) : (
         <ThemedView style={styles.avatarPlaceholder}>
           <ThemedText>{username.charAt(0).toUpperCase()}</ThemedText>
